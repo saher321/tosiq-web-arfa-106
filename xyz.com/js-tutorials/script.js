7377e7f8b7d202e5ee4faf6de808d38fdc4429f4
Tosiq <pnymeet@gmail.com>
2026-02-09 18:54:00 +0500

    js intro

---
+++ xyz.com/js-tutorials/script.js
@@ -0,0 +1,13 @@
+// var, let, const
+var a = 34; // number/int
+var b = 999999999999999999999; // bigInt
+var c = "Tosiq"; // string
+var user = { name: "Awais", age: 4, email: "awais@gmail.com"  }; // object
+// document.write(JSON.stringify(user)); // obj -> str
+
+console.log(user.email);
+
+//            0          1         2      3    4
+var list = ["Item 1", "Item 2", "Item 3", 4, user]; // array
+var isActive = true // false; // boolean
+console.log(list[4]);
