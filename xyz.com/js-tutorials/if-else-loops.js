0ade1ee651ddd73c767461ad665d57ed14851530
Tosiq <pnymeet@gmail.com>
2026-02-10 19:17:20 +0500

    even odd

--- xyz.com/js-tutorials/if-else-loops.js
+++ xyz.com/js-tutorials/if-else-loops.js
@@ -6,10 +6,17 @@
 
 function showResult(){
     var n = Number(document.getElementById("number").value);
-    
-    if(n > 0){
-        console.log("this is Positive number")
+
+    if (n % 2 == 0){
+        console.log("This is even number")
     } else {
-        console.log("This is negative number")
+        console.log("This is odd number")
     }
+    // if(n == 0) {
+    //     console.log("Number is neutral");
+    // } else if(n > 0){
+    //     console.log("this is Positive number")
+    // } else {
+    //     console.log("This is negative number")
+    // }
 }
