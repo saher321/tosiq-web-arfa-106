4c3dade6b5035393d030d8760f7aa9e11944d716
Tosiq <pnymeet@gmail.com>
2026-02-10 18:55:10 +0500

    if else state.

---
+++ xyz.com/js-tutorials/if-else-loops.js
@@ -0,0 +1,15 @@
+// = -> assignment
+// a = 10
+// operator, operants
+// camparison operators
+// >, <, <=, >=, !=, ==, !, ||, &&
+
+function showResult(){
+    var n = Number(document.getElementById("number").value);
+    
+    if(n > 0){
+        console.log("this is Positive number")
+    } else {
+        console.log("This is negative number")
+    }
+}
