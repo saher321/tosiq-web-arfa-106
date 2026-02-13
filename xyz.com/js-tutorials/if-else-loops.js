3a483d69850b1191c4a6bc7da3c4210d25837ed6
Tosiq <pnymeet@gmail.com>
2026-02-13 16:51:20 +0500

    state. loops

--- xyz.com/js-tutorials/if-else-loops.js
+++ xyz.com/js-tutorials/if-else-loops.js
@@ -6,17 +6,30 @@
 
 function showResult(){
     var n = Number(document.getElementById("number").value);
+    console.log(typeof n, n)
+    
+    if (!n) {
+            Toastify({
+            text: "Please provide valid number",
+            duration: 3000
+
+            }).showToast();
+        return;
+    }
 
     if (n % 2 == 0){
         console.log("This is even number")
     } else {
         console.log("This is odd number")
     }
-    // if(n == 0) {
-    //     console.log("Number is neutral");
-    // } else if(n > 0){
-    //     console.log("this is Positive number")
-    // } else {
-    //     console.log("This is negative number")
-    // }
+    if(n == 0) {
+        console.log("Number is neutral");
+    } else if(n > 0){
+        console.log("this is Positive number")
+    } else {
+        console.log("This is negative number")
+    }
+
+
+
 }
