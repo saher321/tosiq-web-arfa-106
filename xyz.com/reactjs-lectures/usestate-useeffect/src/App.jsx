cf7dee657662a165510a0698edbaf484f631a2d6
pny <pnymeet@gmail.com>
2026-03-03 15:44:18 +0500

    useEffect

--- xyz.com/reactjs-lectures/usestate-useeffect/src/App.jsx
+++ xyz.com/reactjs-lectures/usestate-useeffect/src/App.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TabContent from './components/TabContent';
 
 const App = () => {
   const [counter, setCounter] = useState(1);
 
+  const [value, setValue] = useState("Tosiq")
+
   const handleIncreament = () => {
+    
+    setValue("Value is inc.")
+
     if (counter == 5) return;
     setCounter(counter + 1);
+
   }
   const handleDecreament = () => {
+
+    setValue("Value is dec.")
     if (counter == 1) return;
     setCounter(counter - 1);
   }
+
+  useEffect(()=>{
+    console.log("Simple calling")
+  })
+
+  useEffect(()=>{
+    console.log("Run only once")
+  }, [])
+
+  useEffect(()=>{
+    console.log("Runs on condition")
+  }, [value])
+
+
   return (
     <>
-    <div>App Counter</div>
+    <div>App Counter/ {value}</div>
     
     <button onClick={handleDecreament}> - </button> 
     <span>{counter}</span> 
