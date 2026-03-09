722505e3ae11687fe72670add5e672f8d0c32dec
Tosiq <pnymeet@gmail.com>
2026-03-09 15:29:40 +0500

    loading

--- xyz.com/reactjs-lectures/usestate-useeffect/src/pages/Recipes.jsx
+++ xyz.com/reactjs-lectures/usestate-useeffect/src/pages/Recipes.jsx
@@ -2,36 +2,57 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import RecipeItem from '../components/RecipeItem'
 import WebLayout from '../layouts/WebLayout'
+import { CircularProgress } from 'react-loader-spinner'
 
 const Recipes = () => {
 
   const [recipes, setRecipes] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
-    const allRecipes = async () => {
-      try {
-        const response = await axios.get("https://dummyjson.com/recipes")
-        setRecipes(response.data.recipes)
-      } catch (error) {
-        console.log(error)
+      const allRecipes = async () => {
+        try {
+          setIsLoading(true)
+          const response = await axios.get("https://dummyjson.com/recipes")
+          setRecipes(response.data.recipes)
+          setIsLoading(false)
+        } catch (error) {
+          console.log(error)
+          setIsLoading(false)
+        }
       }
-    }
-    allRecipes();
-  }, [])
+      allRecipes();
+    }, [])
 
   return (
     <WebLayout>
-        <div className='grid grid-cols-12 gap-4'>
         {
-        recipes.map((recipe) => {
-            return (
-            <div className='col-span-4' key={recipe.id}>
-                <RecipeItem recipe={recipe} />
-            </div>
-            )
-        })
+          isLoading ?
+          <div className='grid place-items-center'>
+            <CircularProgress
+              height="50"
+              width="50"
+              color="#5478FF"
+              ariaLabel="circular-progress-loading"
+              wrapperStyle={{}}
+              wrapperClass="wrapper-class"
+              visible={true}
+              strokeWidth={2}
+              animationDuration={1}
+              />
+          </div> : 
+          <div className='grid grid-cols-12 gap-4'>
+            {
+            recipes.map((recipe) => {
+              return (
+              <div className='col-span-4' key={recipe.id}>
+                  <RecipeItem recipe={recipe} />
+              </div>
+              )
+              })
+            }
+          </div> 
         }
-        </div>
     </WebLayout>
   )
 }
