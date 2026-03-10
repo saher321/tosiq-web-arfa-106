33a014d1d91b015063c8702141a1addf9aebb933
Tosiq <pnymeet@gmail.com>
2026-03-10 15:31:07 +0500

    react hook form

---
+++ xyz.com/reactjs-lectures/react-form/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { useForm } from 'react-hook-form'
+
+const App = () => {
+
+  const { register, handleSubmit } = useForm();
+
+  const handleUserSubmit = (data) => {
+    axios.post("our-url-will-here", data)
+  }
+
+  return (
+    <div>
+      <h1>React hook form</h1>
+      <form onSubmit={handleSubmit(handleUserSubmit)}>
+        <div>
+          <input {...register("username")} type='text' placeholder='Enter your name' />
+        </div>
+
+        <div>
+          <input {...register("email")} type='email' placeholder='Enter your email' />
+        </div>
+
+        <div>
+          <input {...register("phone")} type='tel' placeholder='Enter your phone' />
+        </div>
+
+        <div>
+          <input {...register("password")} type='password' placeholder='Enter your password' autoComplete='off' />
+        </div>
+
+        <div>
+          <button type='submit'>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
