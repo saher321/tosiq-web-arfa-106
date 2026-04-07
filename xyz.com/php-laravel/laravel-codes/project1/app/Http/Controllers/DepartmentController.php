c5e4c3385aaab3cc9ab2dcfc311a8f0a2e0452bb
Tosiq <pnymeet@gmail.com>
2026-04-07 19:14:37 +0500

    model file created

--- xyz.com/php-laravel/laravel-codes/project1/app/Http/Controllers/DepartmentController.php
+++ xyz.com/php-laravel/laravel-codes/project1/app/Http/Controllers/DepartmentController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Department;
 
 class DepartmentController extends Controller
 {
     //
     function index(){
-        $depts = [
-            [ "id" => 1023, "name" => "Finace" ],
-            [ "id" => 2032, "name" => "Human resource" ],
-            [ "id" => 5042, "name" => "English" ],
-        ];
+        // $depts = [
+        //     [ "id" => 1023, "name" => "Finace" ],
+        //     [ "id" => 2032, "name" => "Human resource" ],
+        //     [ "id" => 5042, "name" => "English" ],
+        // ];
+
+        $depts = Department::all();
         return response()->json($depts);
     }
 }
