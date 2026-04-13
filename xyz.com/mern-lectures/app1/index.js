03150bf2ebb0e4c33f14a08644e37bffc4ba9724
Tosiq <pnymeet@gmail.com>
2026-04-13 19:51:51 +0500

    mern added

---
+++ xyz.com/mern-lectures/app1/index.js
@@ -0,0 +1,3 @@
+// npm init -y
+// pkgs: express, dotenv, nodemon, cors, mongoose, nanoid, nodemailer, bcryptjs, jsonwebtoken, cloudinary, multer, multer-storage-cloudinary
+console.log("Hello from backend!");
